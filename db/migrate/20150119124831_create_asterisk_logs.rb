40df48553b8ffdbd0643c528f63d4f9a49be234d
Shooshpanius <admin@in-da-house.ru>
2015-01-19 18:45:20 +0300

    test

--- db/migrate/20150119124831_create_asterisk_logs.rb
+++ db/migrate/20150119124831_create_asterisk_logs.rb
@@ -2,29 +2,21 @@ class CreateAsteriskLogs < ActiveRecord::Migration
   def change
     create_table :asterisk_logs do |t|
 
-      t.string :eventtype, :limit => 20
-      t.datetime :eventtime
-      t.string :cid_name, :limit => 80
-      t.string :cid_num, :limit => 80
-      t.string :cid_ani, :limit => 80
-      t.string :cid_rdnis, :limit => 80
-      t.string :cid_dnid, :limit => 80
-      t.string :exten, :limit => 80
-      t.string :context, :limit => 80
-      t.string :channame, :limit => 80
+      t.datetime :calldate
+      t.string :clid, :limit => 80
       t.string :src, :limit => 80
       t.string :dst, :limit => 80
+      t.string :dcontext, :limit => 80
       t.string :channel, :limit => 80
       t.string :dstchannel, :limit => 80
-      t.string :appname, :limit => 80
-      t.string :appdata, :limit => 80
+      t.string :lastapp, :limit => 80
+      t.string :lastdata, :limit => 80
+      t.integer :duration
+      t.integer :billsec
+      t.string :disposition, :limit => 45
       t.integer :amaflags
       t.string :accountcode, :limit => 20
       t.string :uniqueid, :limit => 32
-      t.string :linkedid, :limit => 32
-      t.string :peer, :limit => 80
-      t.string :userdeftype, :limit => 255
-      t.string :eventextra, :limit => 255
       t.string :userfield, :limit => 255
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150119124831) do
+ActiveRecord::Schema.define(version: 20141015061237) do
 
   create_table "address_book_corps", force: true do |t|
     t.string   "login"
@@ -26,35 +26,6 @@ ActiveRecord::Schema.define(version: 20150119124831) do
     t.boolean  "active"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-  end
-
-  create_table "asterisk_logs", force: true do |t|
-    t.string   "eventtype",   limit: 20
-    t.datetime "eventtime"
-    t.string   "cid_name",    limit: 80
-    t.string   "cid_num",     limit: 80
-    t.string   "cid_ani",     limit: 80
-    t.string   "cid_rdnis",   limit: 80
-    t.string   "cid_dnid",    limit: 80
-    t.string   "exten",       limit: 80
-    t.string   "context",     limit: 80
-    t.string   "channame",    limit: 80
-    t.string   "src",         limit: 80
-    t.string   "dst",         limit: 80
-    t.string   "channel",     limit: 80
-    t.string   "dstchannel",  limit: 80
-    t.string   "appname",     limit: 80
-    t.string   "appdata",     limit: 80
-    t.integer  "amaflags"
-    t.string   "accountcode", limit: 20
-    t.string   "uniqueid",    limit: 32
-    t.string   "linkedid",    limit: 32
-    t.string   "peer",        limit: 80
-    t.string   "userdeftype"
-    t.string   "eventextra"
-    t.string   "userfield"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
   end
 
   create_table "corp_numbers", force: true do |t|
