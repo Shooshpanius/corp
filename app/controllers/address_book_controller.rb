0ba523f6ffa1915c8858d2eb95a38d286777ba38
Shooshpanius <admin@in-da-house.ru>
2015-01-21 12:34:49 +0300

    test

--- app/controllers/address_book_controller.rb
+++ app/controllers/address_book_controller.rb
@@ -1,5 +1,7 @@
 class AddressBookController < ApplicationController
 
+  before_filter :is_login
+
   require 'socket'      # Sockets are in standard library
 
   def list
@@ -69,6 +71,22 @@ class AddressBookController < ApplicationController
 
     # render nothing: true
 
+  end
+
+
+
+
+  private
+  def is_login
+    if !session[:is_login]
+      user = User.where("auth_hash = ?", cookies[:hash]).take(1)
+      if user.size == 1
+        session[:is_login] = true
+        session[:user_id] = user[0].id
+        session[:user_login] = user[0].login
+        session[:is_admin] = true if user[0].site_admin
+      end
+    end
   end
 
 
