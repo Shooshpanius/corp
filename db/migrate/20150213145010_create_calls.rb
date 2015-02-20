0764e4c8eb76fca1aae4535f16366cb5b093f167
Shooshpanius <admin@in-da-house.ru>
2015-02-20 09:30:49 +0300

    test

--- db/migrate/20150213145010_create_calls.rb
+++ db/migrate/20150213145010_create_calls.rb
@@ -3,19 +3,13 @@ class CreateCalls < ActiveRecord::Migration
     create_table :calls do |t|
 
       t.datetime :calldate
-      t.string :clid, :limit => 80
       t.string :src, :limit => 80
       t.string :dst, :limit => 80
-      t.string :dcontext, :limit => 80
-      t.string :channel, :limit => 80
-      t.string :dstchannel, :limit => 80
-      t.string :lastdata, :limit => 80
       t.integer :duration
       t.integer :billsec
       t.string :disposition, :limit => 45
-
       t.string :uniqueid, :limit => 32
-      t.string :userfield, :limit => 255
+
 
       t.timestamps null: false
     end
