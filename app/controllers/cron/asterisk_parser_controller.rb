24c45ffd33b5dee79aea06ca9d4fc9b74f9a9535
Shooshpanius <admin@in-da-house.ru>
2015-02-20 11:39:58 +0300

    test

--- app/controllers/cron/asterisk_parser_controller.rb
+++ app/controllers/cron/asterisk_parser_controller.rb
@@ -2,6 +2,34 @@ class Cron::AsteriskParserController < ApplicationController
 
   def index
 
+    logs = AsteriskLog.where('parsed  IS NULL')
+
+
+    logs.each do |log_str|
+
+      if log_str.src.to_s.length == 4 && log_str.dst.to_s.length ==4
+        Call.create(
+            calldate: log_str.calldate,
+            src: log_str.src,
+            dst: log_str.dst,
+            duration: log_str.duration,
+            billsec: log_str.billsec,
+            disposition: log_str.disposition,
+            uniqueid: log_str.uniqueid
+        )
+
+        AsteriskLog.update(
+            log_str.id,
+            parsed: true,
+        )
+
+
+      end
+
+
+
+    end
+
     
 
   end
