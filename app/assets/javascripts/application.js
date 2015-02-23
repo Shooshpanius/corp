ca8ff6f8b96577c35b78aadb9ad96c6566b45960
Shooshpanius <admin@in-da-house.ru>
2015-02-23 13:08:32 +0300

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require turbolinks
 //= require jquery.validate
 //= require jquery.validate.additional-methods
-//= require jquery-tablesorter/jquery.metadata
+//= require jquery-tablesorter/extras/jquery.metadata
 //= require jquery-tablesorter/jquery.tablesorter
 //= require jquery-tablesorter/jquery.tablesorter.widgets
 //= require jquery-tablesorter/addons/pager/jquery.tablesorter.pager
