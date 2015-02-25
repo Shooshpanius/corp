348fb5a13b0996c2b83cc17341c84fca6e73e188
Shooshpanius <admin@in-da-house.ru>
2015-02-25 17:05:13 +0300

    Beta

--- app/controllers/cron/asterisk_internal_controller.rb
+++ app/controllers/cron/asterisk_internal_controller.rb
@@ -20,4 +20,22 @@ class Cron::AsteriskInternalController < ApplicationController
     end
   end
 
+  def corp1
+    @form_data = {
+        int_numbers: CorpNumber.where("type_n = ?", 'c')
+    }
+    respond_to do |format|
+      format.text
+    end
+  end
+
+  def corp2
+    @form_data = {
+        int_numbers: CorpNumber.where("type_n = ?", 'c')
+    }
+    respond_to do |format|
+      format.text
+    end
+  end
+
 end
