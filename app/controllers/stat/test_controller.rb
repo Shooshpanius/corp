963dccccad73700ba5658b181bb60a546299a851
Shooshpanius <admin@in-da-house.ru>
2015-03-01 22:37:14 +0300

    Beta

--- app/controllers/stat/test_controller.rb
+++ app/controllers/stat/test_controller.rb
@@ -2,6 +2,25 @@ class Stat::TestController < ApplicationController
 
   def index
 
+    @call = Call.all[5]
+
+    @test = ''
+
+    call_start = @call.calldate - 10
+    call_end = @call.calldate_end + 10
+
+
+    while call_start <= call_end do
+
+      sec = call_start.to_a[0].to_s
+
+      if sec[-1..-1] == '0'
+        @test = @test + ' ' + sec[-1..-1] + ' ' + call_start.to_s + ' | '
+      end
+
+
+      call_start = call_start + 1
+    end
 
   end
 
