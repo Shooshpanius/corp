5b3383999b5e8a3b67cff691ab4c504fa68dccc2
Shooshpanius <admin@in-da-house.ru>
2015-03-04 05:44:36 +0300

    Test

--- app/controllers/admin/autoprovision_controller.rb
+++ app/controllers/admin/autoprovision_controller.rb
@@ -1,4 +1,12 @@
 class Admin::AutoprovisionController < ApplicationController
   def index
+
+    phone_types =  IpPhoneType.all.map { |model| [model.model, model.id] }
+
+
+    @form_data = {
+        phone_types: phone_types
+    }
+
   end
 end
