3352105df098575a1322b97de233c283cccafed1
Shooshpanius <admin@in-da-house.ru>
2015-05-24 20:03:19 +0300

    Beta

--- app/controllers/mailadmin/domains_controller.rb
+++ app/controllers/mailadmin/domains_controller.rb
@@ -1,4 +1,33 @@
 class Mailadmin::DomainsController < ApplicationController
+
+
+  before_filter :is_login
+
+
   def list
+
+    domains = MailDomain.all
+
+    @form_data = {
+        domains: domains
+    }
+
+
   end
+
+
+  private
+  def is_login
+    if !session[:is_login]
+      user = User.where("auth_hash = ?", cookies[:hash]).take(1)
+      if user.size == 1
+        session[:is_login] = true
+        session[:user_id] = user[0].id
+        session[:user_login] = user[0].login
+        session[:is_admin] = true if user[0].site_admin
+      end
+    end
+  end
+
+
 end
