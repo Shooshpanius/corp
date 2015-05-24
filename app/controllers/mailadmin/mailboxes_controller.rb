990c1b28fcbf9f5a8e9735016cfac81e816948af
Shooshpanius <admin@in-da-house.ru>
2015-05-24 22:32:27 +0300

    Beta

--- app/controllers/mailadmin/mailboxes_controller.rb
+++ app/controllers/mailadmin/mailboxes_controller.rb
@@ -1,4 +1,35 @@
 class Mailadmin::MailboxesController < ApplicationController
+  before_filter :is_login
+
   def list
+
+    # if params[:id].to_s.length == 1
+    #   @form_data = {
+    #       mailboxes: MailUser.where('email like ?', params[:id].to_s+'%').order('email ASC'),
+    #   }
+    # elsif params[:id].to_s == 'all'
+    #   @form_data = {
+    #       mailboxes: MailUser.all.order('email ASC'),
+    #   }
+    # end
+
+    @form_data = {
+        mailboxes: MailUser.get_list(params[:id])
+    }
+
   end
+
+  private
+  def is_login
+    if !session[:is_login]
+      user = User.where("auth_hash = ?", cookies[:hash]).take(1)
+      if user.size == 1
+        session[:is_login] = true
+        session[:user_id] = user[0].id
+        session[:user_login] = user[0].login
+        session[:is_admin] = true if user[0].site_admin
+      end
+    end
+  end
+
 end
--- app/models/mail_user.rb
+++ app/models/mail_user.rb
@@ -1,5 +1,38 @@
 class MailUser < ActiveRecord::Base
 
+  def MailUser.get_list(list)
 
+    if list.to_s.length == 1
+      mailboxes = MailUser.where('email like ?', list.to_s+'%').order('email ASC')
+    elsif list.to_s == 'all' or list.to_s.length == 0
+      mailboxes = MailUser.all.order('email ASC')
+    end
+
+    mailboxes.each do |mailbox|
+
+      sotr = AddressBookCorp.where('email = ?', mailbox.email)
+
+      if sotr.size == 1
+        data = {
+            fio: sotr[0].fio,
+            org: sotr[0].organisation
+        }
+      else
+        data = {
+            fio: '-',
+            org: '-'
+        }
+      end
+
+      mailbox.name = data
+
+
+
+    end
+
+
+    return mailboxes
+
+  end
 
 end
