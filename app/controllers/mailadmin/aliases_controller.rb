b75ea9fb67fa3b9711c3f13b7fcc75c3ec340c11
Shooshpanius <admin@in-da-house.ru>
2015-05-28 10:33:07 +0300

    Beta

--- app/controllers/mailadmin/aliases_controller.rb
+++ app/controllers/mailadmin/aliases_controller.rb
@@ -1,4 +1,28 @@
 class Mailadmin::AliasesController < ApplicationController
+
+  before_filter :is_login
+
   def list
+    aliases = MailAlias.all
+
+    @form_data = {
+        aliases: aliases
+    }
+
   end
+
+
+  private
+  def is_login
+    if !session[:is_login]
+      user = User.where("auth_hash = ?", cookies[:hash]).take(1)
+      if user.size == 1
+        session[:is_login] = true
+        session[:user_id] = user[0].id
+        session[:user_login] = user[0].login
+        session[:is_admin] = true if user[0].site_admin
+      end
+    end
+  end
+
 end
