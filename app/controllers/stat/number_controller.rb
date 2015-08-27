fc0212e3aedcf1a45ff34be10fab6d9e00052349
Shooshpanius <admin@in-da-house.ru>
2015-08-27 11:52:35 +0300

    Beta

--- app/controllers/stat/number_controller.rb
+++ app/controllers/stat/number_controller.rb
@@ -7,6 +7,16 @@ class Stat::NumberController < ApplicationController
 
   end
 
+
+  def intern
+
+    @form_data = {
+        corp_numbers: CorpNumber.where("type_n = ? or type_n = ?", 'i', 'a').order('number ASC')
+    }
+
+  end
+
+
   def internal
   end
 end
