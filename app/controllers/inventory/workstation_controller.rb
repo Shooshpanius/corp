2bc8dd74c14ef4e7d17ec96e8db2425c9575712f
Shooshpanius <admin@in-da-house.ru>
2016-01-11 09:27:02 +0300

    Beta

--- app/controllers/inventory/workstation_controller.rb
+++ app/controllers/inventory/workstation_controller.rb
@@ -1,7 +1,13 @@
 class Inventory::WorkstationController < ApplicationController
   def index
+
+    client = Mysql2::Client.new(:host => "zabbix.velskiyles.ru", :username => "ocs", :password => "Vfrfrf1488", :database => "ocsweb")
+    results = client.query("SELECT * FROM bios")
+
+
+
     @form_data = {
-        params: params
+        results: results
     }
   end
 end
