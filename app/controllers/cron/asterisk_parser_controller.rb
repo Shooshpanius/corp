06a2cf52a01b26b8170d070c7481f208aa07d8d3
Shooshpanius <admin@in-da-house.ru>
2016-01-18 13:32:52 +0300

    Beta

--- app/controllers/cron/asterisk_parser_controller.rb
+++ app/controllers/cron/asterisk_parser_controller.rb
@@ -155,6 +155,39 @@ class Cron::AsteriskParserController < ApplicationController
 
       end
 
+
+      # XXX-TD на XXXX c назначением XXXXXXXXXXX (локальный, ушедший на внешний)
+      #
+      if log_str.src.to_s.length == 3 &&
+          log_str.dst.to_s.length ==4 &&
+          log_str.lastdata.to_s.scan(/\d{11}/)[0].to_s.length == 11
+
+        dst = log_str.lastdata.to_s.scan(/\d{11}/)[0].to_s
+        descr = call_descr(dst)
+
+        new_call = Call.create(
+            calldate: log_str.calldate,
+            src: log_str.src,
+            dst: dst,
+            duration: log_str.duration,
+            billsec: log_str.billsec,
+            disposition: log_str.disposition,
+            uniqueid: log_str.uniqueid,
+            direction: 'o',
+            context: log_str.lastdata.to_s.scan(/SIP\/[A-Za-z_-]+\//)[0].to_s[4..-2],
+            descr: descr,
+            calldate_end: log_str.calldate + log_str.duration
+        )
+
+        AsteriskLog.update(
+            log_str.id,
+            parsed: true,
+        )
+
+        # call_to_adv_log(new_call)
+
+      end
+
       # XXXX на XXXXXXXXXXX (на внешний)
       #
       if log_str.src.to_s.length == 4 &&
